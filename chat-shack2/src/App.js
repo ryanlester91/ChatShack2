3870e8a0de39f23a4c1374a1338ea2f66cc46e8c
Ryan Lester <lestertr@dukes.jmu.edu>
2020-10-11 15:43:05 -0400

    Join index page has input boxes for name and room

--- chat-shack2/src/App.js
+++ chat-shack2/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => { return (
     <Router>
